07d9fc80d0345b6dd3faf92fefd4245b14eb1e57
MD. ARIFUL ISLAM <arifhstuece@gmail.com>
2022-03-27 23:31:30 +0600

    js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Microsoft Word", "Microsoft Exel", "Microsoft PowerPoint", "Data Entry"]
+        strings: ["Microsoft Word", "Microsoft Excel", "Microsoft PowerPoint", "Data Entry"]
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
